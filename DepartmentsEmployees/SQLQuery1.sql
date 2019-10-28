34b2f5e7d628a656edba9f5564aec963b42664da
Brian <wilsonedit@gmail.com>
2019-10-28 14:19:42 -0500

    Completed excericse

---
+++ DepartmentsEmployees/SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿SELECT e.Id, e.FirstName, e.LastName, e.DepartmentId, d.DeptName
+                                        FROM Employee e INNER JOIN Department d on d.iD = e.DepartmentId                               
